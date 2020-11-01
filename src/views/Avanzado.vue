ed6104fe0ecf61240e9e1ae72447f74e37fed295
Rodolfo Inostroza <ralphweb@gmail.com>
2020-11-01 20:17:01 -0300

    props finaal

--- src/views/Avanzado.vue
+++ src/views/Avanzado.vue
@@ -1,17 +1,17 @@
 <template>
-<div>
-<header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-lg-8 mx-auto">
-                <div class="post-heading">
-                    <h1><strong>Esta página de administración está en construcción. 
-                    Intente como administrador simple”</strong></h1>
+<div class="avanzado">
+    <header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="post-heading">
+                        <h1><strong>Esta página de administración está en construcción.
+                                Intente como administrador simple”</strong></h1>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 </div>
 </template>
--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -1,16 +1,16 @@
 <template>
-<div>
-<header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-lg-8 mx-auto">
-                <div class="post-heading">
-                    <h1><strong> Bienvenido a la página de Administración</strong></h1>
+<div class="simple"> 
+    <header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="post-heading">
+                        <h1><strong> Bienvenido a la página de Administración</strong></h1>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 </div>
 </template>
